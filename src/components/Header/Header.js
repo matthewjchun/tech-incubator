6bdd6d8d05ca6f8eda80678e64e480f265ff7f42
unknown <matthewchun.18@gmail.com>
2023-02-01 22:14:28 -0500

    dummy header and home basic template created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './Header.css';
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+
+function Home() {
+  
+  return (
+    <div className="dummy-header">
+        sup
+    </div>
+  );
+}
+
+export default Home;
