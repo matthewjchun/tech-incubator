13af7db9e90082d9775fb207fb909b19f07a8862
unknown <matthewchun.18@gmail.com>
2023-02-01 20:42:54 -0500

    template files created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ logged, setLogged ] = useState(false) 
+  
   return (
     <div className="App">
+      {logged ?
+        <p>logged in!</p>
+        :
+        <p>ruh roh</p>
+      }
+
     </div>
   );
 }
