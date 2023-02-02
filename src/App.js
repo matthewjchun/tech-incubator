c06a2154198b38acdbcba234632106c77f20bfc7
unknown <matthewchun.18@gmail.com>
2023-02-01 23:52:48 -0500

    working on modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Home from './components/Home/Home'
 import Header from './components/Header/Header'
+import Create from './components/Modals/Create'
 
 function App() {
   const [ logged, setLogged ] = useState(false) 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="background">
       <Header></Header>
+      <Create></Create>
       {logged ?
         <p>logged in!</p>
         :
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import './Home.css';
+import Create from '../Modals/Create'
 import {
     Button,
     ButtonGroup,
@@ -11,10 +12,13 @@ import {
     Td,
     TableCaption,
     TableContainer,
+    useDisclosure,
 } from '@chakra-ui/react'
 
 
 function Home() {
+    const { createIsOpen, createOnOpen, createOnClose } = useDisclosure();
+
     // fetch the collection data for all of the tasks here, similar to parsume
     // change the text on the button depending on the value of the status parameter: 
     // { task.status ? <Button>Accept</> : <Button>Submit</>}
@@ -23,6 +27,11 @@ function Home() {
     // Try to figure out a better way to deal with overflow in the description cell
     return (
         <div className="home-layout">
+            <Create
+                isOpen={createIsOpen}
+                onOpen={createOnOpen}
+                onClose={createOnClose}
+            ></Create>
             <Button className='home-upload' colorScheme='blue'>Upload New Assignment</Button>
             <TableContainer className="home-table">
                 <Table variant='striped' colorScheme='teal'>
@@ -37,27 +46,27 @@ function Home() {
                     <Tbody>
                         <Tr>
                             <Td>Headstarter</Td>
-                            <Td style={{ maxWidth: "25px"}}>Project #2</Td>
+                            <Td style={{ maxWidth: "25px" }}>Project #2</Td>
                             <Td>Create a tech incubator webpage</Td>
+                            <Td>
+                                <Button colorScheme='blue' onClick={createOnOpen}>Accept</Button>
+                            </Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Google</Td>
+                            <Td style={{ maxWidth: "25px" }}>Hire Matthew Chun</Td>
+                            <Td style={{ maxWidth: "100px", overflowX: "auto" }}>
+                                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt aute id consequat veniam incididunt duis in sint irure nisi. Mollit officia cillum Lorem ullamco minim nostrud elit officia tempor esse quis.
+
+                                Sunt ad dolore quis aute consequat. Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
+                            </Td>
                             <Td>
                                 <Button colorScheme='blue'>Accept</Button>
                             </Td>
                         </Tr>
                         <Tr>
-                            <Td>Google</Td>
-                            <Td style={{ maxWidth: "25px"}}>Hire Matthew Chun</Td>
-                            <Td style={{ maxWidth: "100px", overflowX: "auto" }}>
-                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt aute id consequat veniam incididunt duis in sint irure nisi. Mollit officia cillum Lorem ullamco minim nostrud elit officia tempor esse quis.
-
-Sunt ad dolore quis aute consequat. Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
-                            </Td>
-                            <Td>
-                                <Button colorScheme='blue'>Accept</Button>
-                            </Td>
-                        </Tr>
-                        <Tr> 
                             <Td>Twitter</Td>
-                            <Td style={{ maxWidth: "25px"}}>Fire Elon</Td>
+                            <Td style={{ maxWidth: "25px" }}>Fire Elon</Td>
                             <Td>Please help us hire a new CEO for our company</Td>
                             <Td>
                                 <Button colorScheme='blue'>Accept</Button>
