162729b28bb0ab1926d046428e5590cf420bf392
unknown <matthewchun.18@gmail.com>
2023-02-02 19:56:00 -0500

    finished home modals

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home'
 import Header from './components/Header/Header'
 import Create from './components/Modals/Create'
 import Submit from './components/Modals/Submit'
+import Accept from './components/Modals/Accept'
 
 function App() {
   const [ logged, setLogged ] = useState(false) 
@@ -12,7 +13,8 @@ function App() {
     <div className="background">
       <Header></Header>
       <Create></Create>
-      <Submit></Submit>
+      {/* <Submit></Submit>
+      <Accept></Accept> */}
       {logged ?
         <p>logged in!</p>
         :
--- src/components/Modals/Create.js
+++ src/components/Modals/Create.js
@@ -11,13 +11,15 @@ import {
     Button,
     Input,
     FormControl,
-    FormLabel
+    FormLabel,
+    Heading,
+    Textarea,
 } from '@chakra-ui/react'
 
 
 
 function Create(props) {
-    const { isOpen, onOpen, onClose } = props
+    const { isOpen, onClose } = props
 
     const initialRef = React.useRef(null)
 
@@ -29,23 +31,27 @@ function Create(props) {
         >
             <ModalOverlay />
             <ModalContent>
-                <ModalHeader>Create your account</ModalHeader>
+            <ModalHeader>
+                    <Heading size='lg'>
+                    Upload A New Task
+                    </Heading>
+                </ModalHeader>
                 <ModalCloseButton />
                 <ModalBody pb={6}>
                     <FormControl>
-                        <FormLabel>First name</FormLabel>
-                        <Input ref={initialRef} placeholder='First name' />
+                        <FormLabel>Task name</FormLabel>
+                        <Input ref={initialRef} placeholder='Task name' />
                     </FormControl>
 
                     <FormControl mt={4}>
-                        <FormLabel>Last name</FormLabel>
-                        <Input placeholder='Last name' />
+                        <FormLabel>Description</FormLabel>
+                        <Textarea placeholder='Please write a short description of your task.'/>
                     </FormControl>
                 </ModalBody>
 
                 <ModalFooter>
                     <Button colorScheme='blue' mr={3}>
-                        Save
+                        Upload
                     </Button>
                     <Button onClick={onClose}>Cancel</Button>
                 </ModalFooter>
