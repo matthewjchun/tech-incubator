2f917465cef20952819301db32bd99376ab548f7
unknown <matthewchun.18@gmail.com>
2023-02-05 15:29:58 -0500

    init commit, setting up firebase config

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import { useState } from 'react';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import 'firebase/compat/firestore';
+
 import Home from './components/Home/Home'
 import Header from './components/Header/Header'
-import Create from './components/Modals/Create'
-import Submit from './components/Modals/Submit'
-import Accept from './components/Modals/Accept'
+
+
 
 function App() {
   const [ logged, setLogged ] = useState(false) 
   
+  const firebaseConfig = {
+    apiKey: "AIzaSyBRtmfr_5Z5XkfvuPltYw-40o0LphTk6fU",
+    authDomain: "techubator-483e4.firebaseapp.com",
+    projectId: "techubator-483e4",
+    storageBucket: "techubator-483e4.appspot.com",
+    messagingSenderId: "692270211573",
+    appId: "1:692270211573:web:f32f3b5570e6cdff60c0bc",
+    measurementId: "G-785NKCN8N6"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
   return (
     <div className="background">
       <Header></Header>
-      <Create></Create>
-      {/* <Submit></Submit>
-      <Accept></Accept> */}
       {logged ?
         <p>logged in!</p>
         :
